edbd9f2488a188390a1704cb7f661bb5b554d354
byCALLlA <bycallla@ya.ru>
2022-05-21 08:15:00 +0300

    черновой функционал

---
+++ src/components/RepoNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RepoNotFound = () => {
+    return (
+        <div>
+            Публичные репозитории у этого пользователя отсутствуют
+        </div>
+    );
+};
+
+export default RepoNotFound;
---
+++ src/components/UserNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserNotFound = () => {
+    return (
+        <div>
+            Пользователь с таким именем не найден.
+        </div>
+    );
+};
+
+export default UserNotFound;
