66536a8ce452f91e1eb72d96b79abd0e953d2d2a
byCALLlA <bycallla@ya.ru>
2022-05-23 23:45:56 +0300

    c черновой версткой

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Repositories from "./components/Repositories";
 
 function App() {
     const [initialState, setInitialState] = useState(true)
-    const [isLoaded, setIsLoaded] = useState(false)
+    //const [isLoaded, setIsLoaded] = useState(false)
     const [user, setUser] = useState('')
     const [repo, setRepo] = useState([])
     const [username, setUsername] = useState('')
@@ -32,16 +32,16 @@ function App() {
     }
 
     const fetchRepo = (currentPage) => {
-        setIsLoaded(true)
+        //setIsLoaded(true)
         fetch(`https://api.github.com/users/${username}/repos?per_page=4&page=${currentPage}`)
             .then(response => response.json())
             .then(json => setRepo(json))
-        setIsLoaded(false)
+            //setIsLoaded(false)
     }
 
     const [pageCount, setPageCount] = useState(0);
     const [itemOffset, setItemOffset] = useState(0);
-    const [itemsPerPage, setItemsPerPage] = useState(4)
+    const itemsPerPage = 4
     const handlePageClick = (event) => {
         const newOffset = (event.selected * itemsPerPage) % user['public_repos'];
         console.log(
@@ -61,9 +61,9 @@ function App() {
     }
 
     useEffect(() => {
-        const endOffset = itemOffset + itemsPerPage;
+        //const endOffset = itemOffset + itemsPerPage;
         setPageCount(Math.ceil(user['public_repos'] / itemsPerPage));
-    }, [user, itemOffset]);
+    }, [user, itemOffset, itemsPerPage]);
 
     return (
     <div className="App">
